4549a8f370ee8b72b85169a90572acc6f96fbe3a
Moisés Soto <musoto96@gmail.com>
2021-03-29 13:46:23 -0600

    Fixed from previous template

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import logo from './sbr-logo.png';
 import KeyUpload from './upload/KeyUpload.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <KeyUpload />
-        <a className="App-link" href="https://sbr-mx.com" target="_blank" rel="noopener noreferrer">
-          Ir a página de inicio
+        <a className="App-link" href="https://github.com/musoto96" target="_blank" rel="noopener noreferrer">
+          github musoto96
         </a>
       </header>
     </div>
